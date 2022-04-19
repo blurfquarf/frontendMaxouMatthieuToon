3daac302692f43af70915036afd04b8b5a4e5c05
MatthieuDL <matthieu.de.langhe1@gmail.com>
2022-04-19 11:19:55 +0200

    cards in grid shape

--- src/components/show-subject.component.js
+++ src/components/show-subject.component.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import subjectService from "../services/subject.service";
 import {connect} from "react-redux";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Container, Row, Col
+} from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class ShowSubject extends Component {
@@ -37,6 +41,29 @@ export default class ShowSubject extends Component {
         const {content} = this.state;
         console.log(content)
         return (
+            <Container>
+                <Row xs={3}>
+                    {content.filter((content) => content.approved === true).map(content => {
+                        return (
+                            <Col>
+                                <Card>
+                                    <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap"/>
+                                    <CardBody>
+                                        <CardTitle tag="h5">{content.title}</CardTitle>
+                                        <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                                        <CardText>
+                                            {content.description}
+                                        </CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Container>
+
+            /*
             <div>
                 <h3>Goedgekeurde onderwerpen</h3>
                 <div className="row">
@@ -55,6 +82,7 @@ export default class ShowSubject extends Component {
                     }
                 </div>
             </div>
+             */
         );
     }
 }
